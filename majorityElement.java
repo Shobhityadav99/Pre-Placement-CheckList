ab9d16a8b6aa545fb8b824763ba6d078226c71de
Shobhit Yadav <shobhit999000@gmail.com>
2020-12-31 13:00:00 +0530

    fourth code added

--- majorityElement.java
+++ majorityElement.java
@@ -1,10 +1,51 @@
 package prePlacementCheckList;
 
+import java.util.Scanner;
+
 public class majorityElement {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		int cand=findCandidate(arr,n);
+		if(isMajority(arr,n,cand)) {
+			System.out.println(cand);
+		}
+		else
+			System.out.println("No Such Element");
+		}
 
+	static int findCandidate(int arr[],int n) {
+		int maj_index=0;
+		int count=1;
+		for(int i=1;i<n;i++) {
+			if(arr[maj_index]==arr[i])
+				count++;
+			else
+				count--;
+			if(count==0) {
+				maj_index=i;
+				count=1;
+			}
+		}
+		return arr[maj_index];
 	}
-
+	
+	static boolean isMajority(int arr[],int n,int cand) {
+		int count=0;
+		for(int i=0;i<n;i++) {
+			if(arr[i]==cand) {
+				count++;
+			}
+		}
+		if(count>n/2) {
+			return true;
+		}
+		return false;
+	}
 }
+		
---
+++ rotateby90Degree.java
@@ -0,0 +1,10 @@
+package prePlacementCheckList;
+
+public class rotateby90Degree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
